4e5754e8e40b4dde5c5d17577b9d30c55d707580
Paul J. Park <paul@idealworldinc.com>
2022-11-10 16:50:02 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+(function _hooks_init(port, debug) {
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const axios = require('axios').default;
+    var Mastodon = require('mastodon');
+
+    const appConfig = {
+	mastodonRoute: '/mastodon/postPublish',
+	mastodonAccessToken: 'MbNqc0IEWdYWsCir9FAegQXcWBDq_94AeuqULJZw7dY',
+	mastodonAPIUrl: 'https://mas.to/api/v1/',
+    };
+
+    function dbg(msg) {
+	if (debug) {
+	    console.log(msg);
+	}
+    }
+
+    const app = express();
+    var m = new Mastodon({
+	access_token: appConfig.mastodonAccessToken,
+	api_url: appConfig.mastodonAPIUrl
+    });
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.get('/', (req, res) => res.send('hook responder present.'));
+    app.get('/ayt', (req, res) => res.send('Yes.'));
+    app.post(appConfig.mastodonRoute, (req, res) => {
+	dbg('postpublish to mastodon: ' + JSON.stringify(req.body));
+	let postParameters = {
+	    status: req.body.status,
+	};
+	if (req.body.feature_image) {
+	    axios.get(req.body.feature_image, {
+		responseType: 'stream'
+	    }).then((aResp) => {
+		m.post('/media', { file: aResp.data }).then((mmResp) => {
+		    dbg('postpublish to mastodon media: ' +
+			JSON.stringify(mmResp.data));
+		    if (mmResp && mmResp.data && !mmResp.data.error) {
+			if (mmResp.data.id) {
+			    postParameters.media_ids = [ mmResp.data.id ];
+			}
+			m.post('/statuses', postParameters).then((msResp) => {
+			    if (msResp && msResp.data) {
+				dbg('postpublish mastodon status w/media: ' +
+				    JSON.stringify(msResp.data));
+				res.send(msResp.data);
+			    } else {
+				res.send(400);
+			    }
+			});
+		    } else {
+			res.send((mmResp && mmResp.data) ?
+				 mmResp.data.error : 401);
+		    }
+		});
+	    });
+	} else {
+	    m.post('/statuses', postParameters).then((mResp) => {
+		if (mResp && mResp.data) {
+		    dbg('postpublish mastodon status only success: ' +
+			JSON.stringify(mResp.data));
+		    res.send(mResp.data);
+		} else {
+		    res.send(400);
+		}
+	    });
+	}
+    });
+    app.listen(port, () => {
+	dbg('hooks listening on port ' + port);
+    });
+})(process.env.PORT || 3001,
+   process.env.DEBUG || 0);
