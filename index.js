229cfffb920d24f40808822e450b7ecb8a3f17d2
Paul J. Park <paul@intrepid18.com>
2023-06-15 12:16:19 -0400

    Add rudimentary support to post to nostr, add rudimentary post checks, add tag filters

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 (function _hooks_init(port, debug) {
+    const fs = require('fs').promises;
     const express = require('express');
     const bodyParser = require('body-parser');
     const axios = require('axios');
+    const app = express();
     var Mastodon = require('mastodon');
+    const ws = require('websocket-polyfill');
+    var nostr = require('nostr-tools');
+    const nhmarkdown = require('node-html-markdown');
+    const bech32 = require('bech32');
+    const nhm = new nhmarkdown.NodeHtmlMarkdown();
+    var crypto = require('crypto');
+    crypto.getRandomValues = require('get-random-values');
 
-    const appConfig = {
-	mastodonRoute: '/mastodon/postPublish',
-	mastodonAccessToken: 'Gt07ox7817B9rsKX4icC_6s7bV-LktUsGc-B3c-k9CA',
-	mastodonAPIUrl: 'https://mas.to/api/v1/',
-	ghostReplayTimeout: 60,
-	mastodonMaxStatusLength: 450,
+    const defaultConfig = {
+	valid: false,
+	replayTimeout: 60,
     };
 
     function dbg(msg) {
@@ -20,107 +26,219 @@
     function nullPromise(ret) {
 	return(new Promise((npres, nprej) => { npres(ret); }));
     }
-    function makeMastodonStatus(gp) {
-	let s = gp.plaintext;
-	if (gp.url) {
-	    s = 'Original: ' + gp.url + '\n' + s;
-	}
-	if (s.length > appConfig.mastodonMaxStatusLength) {
-	    s = s.substring(0, appConfig.mastodonMaxStatusLength - 3) + '...';
-	}
-	return(s);
-    }
 
-    const app = express();
-    var m = new Mastodon({
-	access_token: appConfig.mastodonAccessToken,
-	api_url: appConfig.mastodonAPIUrl
-    });
-    var inProgress = {};
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.get('/', (req, res) => res.send('hook responder present.'));
-    app.get('/ayt', (req, res) => res.send('Yes.'));
-    app.post(appConfig.mastodonRoute, (req, res) => {
-	dbg('postpublish to mastodon: ' +
-	    JSON.stringify(req.body.post.current));
-	if (req.body.post.current.id) {
-	    let ghostID = req.body.post.current.id;
-	    if (inProgress[ghostID]) {
-		if (inProgress[ghostID].done) {
-		    dbg('postpublish already complete for ' + ghostID);
-		    res.status(inProgress[ghostID].status).
-			send(inProgress[ghostID].data);
-		} else {
-		    dbg('postpublish in progress ' + ghostID);
-		    res.end();
+    const targets = {
+	mastodon: {
+	    handler: (protoConfig, post) => {
+		var m = new Mastodon({
+		    access_token: protoConfig._accessToken,
+		    api_url: protoConfig._APIUrl
+		});
+		function makeMastodonStatus(gp) {
+		    let s = gp.plaintext;
+		    if (gp.url) {
+			s = 'Original: ' + gp.url + '\n' + s;
+		    }
+		    if (s.length > protoConfig._maxStatusLength) {
+			s = s.substring(0, protoConfig._maxStatusLength - 3) + '...';
+		    }
+		    return(s);
 		}
-	    } else {
-		let postParameters = {
-		    status: makeMastodonStatus(req.body.post.current),
-		};
-		inProgress[ghostID] = {
-		    done: false,
-		    status: 500,
-		};
-		let tlp;
-		if (req.body.post.current.feature_image) {
-		    dbg('getting ' + req.body.post.current.feature_image);
-		    tlp = axios.get(req.body.post.current.feature_image, {
+		let doit = protoConfig._doit;
+		if (doit && protoConfig._tags &&
+		    Array.isArray(protoConfig._tags) &&
+		    protoConfig._tags.length) {
+		    doit = (post.tags && Array.isArray(post.tags) &&
+			    post.tags.length) ?
+			protoConfig._tags.some((_t) => {
+			    return(post.tags.find((_e) => {
+				return(_e.name == _t) }) ? true : false)}) : 
+			false;
+		    if (!doit) {
+			dbg('post rejected: no required tag present');
+		    }
+		}
+		if (doit) {
+		    let postParameters = {
+			status: makeMastodonStatus(post),
+		    };
+		    let tlp;
+		    if (post.feature_image) {
+			dbg(`getting ${post.feature_image}`);
+			tlp = axios.get(post.feature_image, {
 			    responseType: 'stream'
+			});
+		    } else {
+			dbg('no image');
+			tlp = nullPromise({nomedia:true});
+		    }
+		    tlp.then((aResp) => {
+			if (aResp.nomedia) {
+			    dbg('no image 2');
+			    return(nullPromise({nomedia:true}));
+			} else {
+			    dbg('mastodon posting image');
+			    return(m.post('/media', { file: aResp.data }));
+			}
+		    }).then((mmResp) => {
+			dbg('postpublish to mastodon media: ' +
+			    JSON.stringify(mmResp.data));
+			if (mmResp.data && mmResp.data.id) {
+			    postParameters.media_ids = [ mmResp.data.id ];
+			}
+			return(m.post('/statuses', postParameters));
+		    }).then((msResp) => {
+			if (msResp && msResp.data) {
+			    dbg('postpublish mastodon status: ' +
+				JSON.stringify(msResp.data));
+			} else {
+			    dbg('postpublish mastodon status: no response data');
+			}
+		    }).catch((error) => {
+			dbg('postpublish mastodon status failure: ' +
+			    JSON.stringify(error));
 		    });
 		} else {
-		    dbg('no image');
-		    tlp = nullPromise({nomedia:true});
+		    console.log('Would have posted to mastodon: ' +
+				JSON.stringify(post));
 		}
-		tlp.then((aResp) => {
-		    if (aResp.nomedia) {
-			dbg('no image 2');
-			return(nullPromise({nomedia:true}));
+	    }
+	},
+	nostr: {
+	    handler: (protoConfig, post) => {
+		let doit = protoConfig._doit;
+		if (doit && protoConfig._tags &&
+		    Array.isArray(protoConfig._tags) &&
+		    protoConfig._tags.length) {
+		    doit = (post.tags && Array.isArray(post.tags) &&
+			    post.tags.length) ?
+			protoConfig._tags.some((_t) => {
+			    return(post.tags.find((_e) => {
+				return(_e.name == _t) }) ? true : false)}) :
+			false;
+		    if (!doit) {
+			dbg('post rejected: no required tag present');
+		    }
+		}
+		if (doit) {
+		    let event = {
+			kind: 30023,
+			pubkey: protoConfig._pk,
+			created_at: Math.floor((post.created_at ? (new Date(post.created_at)) : Date.now()) / 1000),
+			tags: [],
+			title: post.title,
+			image: post.feature_image,
+			content: nhm.translate(post.html),
+		    };
+		    try {
+			let pko = bech32.bech32.decode(protoConfig._pk);
+			if (pko.prefix == 'npub') {
+			    event.pubkey = bech32.bech32.fromWords(pko.words).reduce(
+				(_a,_cv) => {
+				    _a = _a + _cv.toString(16).padStart(2, '0'); return(_a);
+				}, '');
+			}
+		    }
+		    catch (pkerr) {
+		    }
+		    event.id = nostr.getEventHash(event);
+		    let sk = protoConfig._sk;
+		    try {
+			let sko = bech32.bech32.decode(protoConfig._sk);
+			if (sko.prefix == 'nsec') {
+			    sk = bech32.bech32.fromWords(sko.words).reduce(
+				(_a,_cv) => {
+				    _a = _a + _cv.toString(16).padStart(2, '0'); return(_a);
+				}, '');
+			}
+		    }
+		    catch (skerr) {
+			console.error('error making sk: ' + skerr.message);
+		    }
+		    event.sig = nostr.getSignature(event, sk);
+		    const relay = nostr.relayInit(protoConfig._relay);
+		    relay.on('connect', () => {
+			dbg('got relay connected dispatch');
+		    });
+		    relay.on('error', () => {
+			console.error(`failed to connect to ${protoConfig._relay}`);
+			relay.close();
+		    });
+		    relay.connect().then(() => {
+			dbg(`connected to ${protoConfig._relay}`);
+			let pub = relay.publish(event);
+			pub.on('ok', () => {
+			    console.log(`${protoConfig._relay} has accepted our event`);
+			    relay.close();
+			});
+			pub.on('failed', reason => {
+			    console.log(`failed to publish to ${protoConfig._relay}: ${reason}`);
+			    relay.close();
+			});
+		    });
+		} else {
+		    console.log('Would have posted to nostr: ' +
+				JSON.stringify(post));
+		}
+	    },
+	},
+    };
+
+    async function _doit() {
+	var config = defaultConfig;
+	var configFile = process.env.CONFIG || './config.json';
+	try {
+	    const readBuffer = await fs.readFile(configFile);
+	    config = JSON.parse(readBuffer);
+	    if (!config.replayTimeout) {
+		config.replayTimeout = 180;
+	    }
+	    config.valid = true;
+	} catch (cErr) {
+	    console.error(`Cannot read configuration: ${cErr.message}`);
+	    console.error(JSON.stringify(cErr));
+	}
+	if (config.valid) {
+	    app.use(bodyParser.urlencoded({ extended: false }));
+	    app.use(bodyParser.json());
+	    app.get('/', (req, res) => res.send('hook responder present.'));
+	    app.get('/ayt', (req, res) => res.send('Yes.'));
+
+	    if (config.targets && Array.isArray(config.targets)) {
+		config.targets.forEach((_t) => {
+		    if (targets[_t.protocol]) {
+			app.post(_t._route, (req, res) => {
+			    if (req.body.post.current.id) {
+				if (!config.urlCheck ||
+				    req.body.post.current.url.startsWith(config.urlCheck)) {
+				    dbg(`postpublish ${req.body.post.current.id} to ${_t.protocol}`);
+				    res.status(200).send();
+				    targets[_t.protocol].handler(_t, req.body.post.current);
+				} else {
+				    res.status(400).send({message: 'post rejected'});
+				    console.error(`post ${req.body.post.current.url} rejected, does not match ${config.urlCheck}`);
+				}
+			    } else {
+				res.status(400).send({message: 'no post identifier'});
+				console.error('No post identifier in ' +
+					      JSON.stringify(req.body.post.current));
+			    }
+			});
 		    } else {
-			dbg('mastodon posting image');
-			return(m.post('/media', { file: aResp.data }));
+			console.error(`No target entry for ${_t.protocol}`);
 		    }
-		}).then((mmResp) => {
-		    dbg('postpublish to mastodon media: ' +
-			JSON.stringify(mmResp.data));
-		    if (mmResp.data && mmResp.data.id) {
-			postParameters.media_ids = [ mmResp.data.id ];
-		    }
-		    return(m.post('/statuses', postParameters));
-		}).then((msResp) => {
-		    inProgress[ghostID].done = true;
-		    if (msResp && msResp.data) {
-			dbg('postpublish mastodon status: ' +
-			    JSON.stringify(msResp.data));
-			inProgress[ghostID].status = 200;
-			inProgress[ghostID].data = msResp.data;
-		    } else {
-			dbg('postpublish mastodon status: no response data');
-			inProgress[ghostID].status = 400;
-			inProgress[ghostID].data =
-			    { error: 'mastodon publish missing status' };
-		    }
-		}).catch((error) => {
-		    dbg('postpublish mastodon status failure: ' +
-			JSON.stringify(error));
-		    inProgress[ghostID].done = true;
-		    inProgress[ghostID].status = 500;
-		    inProgress[ghostID].data = error;
-		}).finally(() => {
-		    res.status(inProgress[ghostID].status).send(inProgress[ghostID].data);
-		    setTimeout(() => {
-			dbg('cleaning up replay entry for: ' + ghostID);
-			delete(inProgress[ghostID]);
-		    }, appConfig.ghostReplayTimeout * 1000);
 		});
 	    }
+	    app.listen(port, () => {
+		dbg(`hooks listening on port ${port}`);
+	    });
 	} else {
-	    res.status(500).send({error: 'no ghost post id'});
+	    console.error('Invalid configuration.');
+	    return(1);
 	}
-    });
-    app.listen(port, () => {
-	dbg('hooks listening on port ' + port);
-    });
+	return(0);
+    }
+    
+    _doit();
 })(process.env.PORT || 3001,
    process.env.DEBUG || 0);
